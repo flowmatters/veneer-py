c5bab68b4fe2fbb9eb33797e77fb64a5e94ee4ba
Joel Rahman <joel@flowmatters.com.au>
2016-05-08 06:20:36 +1000

    Brought retrieve all functionality in so can be used in scripts and notebooks

--- veneer.py
+++ veneer.py
@@ -810,3 +810,124 @@ class SearchableList(object):
             return lambda: {k:self.__getattr__(FIND_PREFIX+field_name)(k) for k in self._unique_values(field_name)}
         raise AttributeError(name + ' not allowed')
 
+def read_sdt(fn):
+    import pandas as pd
+    ts = pd.read_table(fn,sep=' +',engine='python',names=['Year','Month','Day','Val'])
+    ts['Date'] = ts.apply(lambda row: pd.datetime(int(row.Year),int(row.Month),int(row.Day)),axis=1)
+    ts = ts.set_index(ts.Date)
+    return ts.Val
+
+def to_source_date(the_date):
+    if hasattr(the_date,'strftime'):
+        return the_date.strftime('%d/%m/%Y')
+    return the_date
+
+class VeneerRetriever(object):
+    def __init__(self,destination,port=9876,host='localhost',protocol='http',
+                 retrieve_daily=True,retreive_monthly=True,retrieve_annual=True,
+                 retrieve_slim_ts=True,print_all = False, print_urls = True):
+        self.destination = destination
+        self.port = port
+        self.host = host
+        self.protocol = protocol
+        self.retrieve_daily = retrieve_daily
+        self.retreive_monthly = retreive_monthly
+        self.retrieve_annual = retrieve_annual
+        self.retrieve_slim_ts = retrieve_slim_ts
+        self.print_all = print_all
+        self.print_urls = print_urls
+        self.base_url = "%s://%s:%d" % (protocol,host,port)
+        self._veneer = Veneer(host=self.host,port=self.port,protocol=self.protocol)
+
+    def mkdirs(self,directory):
+        import os
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    def save_data(self,base_name,data,ext,mode="b"):
+        import os
+        base_name = os.path.join(self.destination,base_name + "."+ext)
+        directory = os.path.dirname(base_name)
+        self.mkdirs(directory)
+        f = open(base_name,"w"+mode)
+        f.write(data)
+        f.close()
+    
+    def retrieve_json(self,url,**kwargs):
+        if self.print_urls:
+            print("*** %s ***" % (url))
+    
+        text = urlopen(self.base_url + quote(url)).read().decode('utf-8')
+        self.save_data(url[1:],bytes(text,'utf-8'),"json")
+    
+        if self.print_all:
+            print(json.loads(text))
+            print("")
+        return json.loads(text)
+    
+    def retrieve_resource(self,url,ext):
+        if self.print_urls:
+            print("*** %s ***" % (url))
+    
+        self.save_data(url[1:],urlopen(self.base_url+quote(url)).read(),ext,mode="b")
+    
+    
+    # Process Run list and results
+    def retrieve_runs(self):
+        run_list = self.retrieve_json("/runs")
+        for run in run_list:
+            run_results = self.retrieve_json(run['RunUrl'])
+            ts_results = run_results['Results']
+            for result in ts_results:
+                self.retrieve_ts(result['TimeSeriesUrl'])
+
+            if self.retrieve_slim_ts:
+                self.retrieve_multi_ts(ts_results)
+
+    def retrieve_multi_ts(self,ts_results):
+        recorders = list(set([(r['RecordingElement'],r['RecordingVariable']) for r in ts_results]))
+        for r in recorders:
+            for option in ts_results:
+                if option['RecordingElement'] == r[0] and option['RecordingVariable'] == r[1]:
+                    url = option['TimeSeriesUrl'].split('/')
+                    url[4] = '__all__'
+                    url = '/'.join(url)
+                    self.retrieve_ts(url)
+                    break
+
+    def retrieve_ts(self,ts_url):
+        if self.retrieve_daily:
+            self.retrieve_json(ts_url)
+        if self.retreive_monthly:
+            self.retrieve_json(ts_url + "/aggregated/monthly")
+        if self.retrieve_annual:
+            self.retrieve_json(ts_url + "/aggregated/annual")
+    
+    def retrieve_variables(self):
+        variables = self.retrieve_json("/variables")
+        for var in variables:
+            if var['TimeSeries']: self.retrieve_json(var['TimeSeries'])
+            if var['PiecewiseFunction']: self.retrieve_json(var['PiecewiseFunction'])
+
+    def retrieve_all(self,destination,**kwargs):
+        self.mkdirs(self.destination)
+        self.retrieve_runs()
+        self.retrieve_json("/functions")
+        self.retrieve_variables()
+        self.retrieve_json("/inputSets")
+        self.retrieve_json("/")
+        network = self.retrieve_json("/network")
+        icons_retrieved = []
+        for f in network['features']:
+            #retrieve_json(f['id'])
+            if not f['properties']['feature_type'] == 'node': continue
+            if f['properties']['icon'] in icons_retrieved: continue
+            self.retrieve_resource(f['properties']['icon'],'png')
+            icons_retrieved.append(f['properties']['icon'])
+
+if __name__ == '__main__':
+    # Output
+    destination = sys.argv[1] if len(sys.argv)>1 else "C:\\temp\\veneer_download\\"
+    print("Downloading all Veneer data to %s"%destination)
+    retriever = VeneerRetriever(destination)
+    retriever.retrieve_all(destination)
